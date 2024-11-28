5db566130635a7bf5090e032e767b6482b2298d2
joseca48 <jose.calizaya.poma@cftmail.cl>
2024-11-28 11:51:47 -0300

    comentario de esta instancia

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { UbicanosComponent } from './componentes/ubicanos/ubicanos.component';
+import { ContactanosComponent } from './componentes/contactanos/contactanos.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    UbicanosComponent,
+    ContactanosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
